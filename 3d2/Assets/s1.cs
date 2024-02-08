0aeb0e72fad435380df63445b4cd8fab551bc7c7
QI JUN HE <heqijun130@gmail.com>
2024-02-08 02:48:09 -0800

    add
    add

---
+++ 3d2/Assets/s1.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s1 : MonoBehaviour
+{
+    public string myMessage; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("Hello World");
+        Debug.Log(myMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
